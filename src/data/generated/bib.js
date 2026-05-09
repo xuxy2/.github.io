23615f3d55d374c90f3fab72002c82449ef999cf
xuxy2 <111361103+xuxy2@users.noreply.github.com>
2026-05-09 21:28:12 +0100

    Add files via upload

---
+++ src/data/generated/bib.js
@@ -0,0 +1,127 @@
+﻿const generatedBibEntries = {
+    "Bahrin2025": {
+        "abstract": "Reports mean immersion score of 8.37/10 and links high immersion to visual fidelity and responsive feedback.",
+        "author": "Bahrin, Abdul Syafiq and Sunar, Mohd Shahrizal and Latip, Hadafi Fitri Mohd and Gopalan, Valarmathie",
+        "doi": "10.32890/jcisc2025.4.12",
+        "journal": "Journal of Creative Industry and Sustainable Culture",
+        "keywords": "quantitative, qualitative, immersion_measured",
+        "title": "Evaluating Perceived Immersiveness in a Self-Competition VR Cycling Exergame Post-Prototyping Enhancements",
+        "type": "article",
+        "volume": "4",
+        "year": "2025"
+    },
+    "Berglund2024": {
+        "abstract": "Quasi-experimental study comparing upper-body vs full-body and sitting vs standing interactions on perceived control and immersion.",
+        "author": "Berglund, Aseel and Or\u00e4dd, Helena",
+        "doi": "10.2196/55905",
+        "journal": "JMIR Serious Games",
+        "keywords": "quasi_experiment, immersion_perceived",
+        "title": "Exploring the Psychological Effects and Physical Exertion of Using Different Movement Interactions in Casual Exergames That Promote Active Microbreaks",
+        "type": "article",
+        "year": "2024"
+    },
+    "Colombo2025": {
+        "abstract": "Compares four immersion levels (no VR, VR TV, VR HMD, VR HMD+interaction) and finds highest immersion with interaction increases breathing rate and perceived effort.",
+        "author": "Colombo, Vera and Mondellari, Marta and Aliverti, Andrea and Sacco, Marco",
+        "doi": "10.3389/frvir.2025.1490588",
+        "journal": "Frontiers in Virtual Reality",
+        "keywords": "quantitative, user_study, immersion_perceived",
+        "title": "Immersion and Interaction During Cycling in Virtual Reality: The Influence on Perceived Effort and Subjective Experience",
+        "type": "article",
+        "volume": "6",
+        "year": "2025"
+    },
+    "Faric2019": {
+        "abstract": "Thematic analysis of 498 user reviews reveals that immersion distracts from discomfort, making exercise more enjoyable.",
+        "author": "Faric, N. and Potts, H. W. W. and Hon, A. and Smith, L. and Newby, K. and Steptoe, A. and Fisher, A.",
+        "doi": "10.2196/13833",
+        "journal": "Journal of Medical Internet Research",
+        "keywords": "qualitative, immersion_perceived",
+        "number": "9",
+        "pages": "e13833",
+        "title": "What Players of Virtual Reality Exercise Games Want: Thematic Analysis of Web-Based Reviews",
+        "type": "article",
+        "volume": "21",
+        "year": "2019"
+    },
+    "Gu2025": {
+        "abstract": "Shows that high-intensity VR exergaming improves executive function and mood more than moderate intensity.",
+        "author": "Gu, Q. and Mao, J. and Sun, J. and Teo, W. P. and others",
+        "doi": "10.1016/j.physbeh.2024.114719",
+        "journal": "Physiology \\& Behavior",
+        "keywords": "quantitative, user_study",
+        "pages": "114719",
+        "title": "Exercise Intensity of Virtual Reality Exergaming Modulates the Responses to Executive Function and Affective Response in Sedentary Young Adults",
+        "type": "article",
+        "volume": "288",
+        "year": "2025"
+    },
+    "Hein2025": {
+        "abstract": "Uses heart rate sensors to dynamically adjust VR cycling, helping users maintain target exercise zones.",
+        "author": "Hein, Oliver and Wackerl, Sandra and Ou, Changkun and Alt, Florian and Chiossi, Francesco",
+        "booktitle": "ACM International Conference on Sports and Human-Computer Interaction (SportsHCI)",
+        "doi": "10.1145/3749385.3749398",
+        "keywords": "quantitative, user_study, bioadaptive",
+        "title": "At the Speed of the Heart: Evaluating Physiologically-Adaptive Visualizations for Supporting Engagement in Biking Exergaming in Virtual Reality",
+        "type": "inproceedings",
+        "year": "2025"
+    },
+    "Kamath2025": {
+        "abstract": "Co-designed with BLV users; all participants reached moderate-to-vigorous physical activity in VR boxing.",
+        "author": "Kamath, Sanchita S. and others",
+        "booktitle": "ACM CHI Conference on Human Factors in Computing Systems",
+        "doi": "10.1145/3663547.3746365",
+        "keywords": "co_design, quantitative, user_study, inclusive",
+        "title": "PunchPulse: A Physically Demanding Virtual Reality Boxing Game Designed with, for and by Blind and Low-Vision Players",
+        "type": "inproceedings",
+        "year": "2025"
+    },
+    "Marshall2021": {
+        "abstract": "This scoping review examines whether exergames produce sufficient short-term exercise intensity. Analyzing 243 studies from 3171 records, it finds high variability in intensity and criticizes control conditions that inflate effects.",
+        "author": "Marshall, Joe and Linehan, Conor",
+        "doi": "10.1109/TG.2020.2995370",
+        "journal": "IEEE Transactions on Games",
+        "keywords": "scoping_review, qualitative",
+        "number": "2",
+        "pages": "160--169",
+        "title": "Are Exergames Exercise? A Scoping Review of the Short-Term Effects of Exertion Games",
+        "type": "article",
+        "volume": "13",
+        "year": "2021"
+    },
+    "MartinNiedecken2021": {
+        "abstract": "Proposes a design framework for balancing game enjoyment and physical exertion, including a taxonomy of exergame intensity levels.",
+        "author": "Martin-Niedecken, Anna Lisa",
+        "isbn": "978-3-907209-12-4",
+        "keywords": "design_framework, taxonomy",
+        "note": "Doctoral dissertation",
+        "school": "University of Applied Sciences and Arts Northwestern Switzerland",
+        "title": "Towards Balancing Fun and Exertion in Exergames",
+        "type": "phdthesis",
+        "year": "2021"
+    },
+    "Naugle2024": {
+        "abstract": "Investigates the discrepancy between actual (heart rate) and perceived (RPE) exercise intensity in VR active games.",
+        "author": "Naugle, Keith E. and Cervantes, Xzaliya A. and Boone, Carolyn L. and Wind, Brandon and Naugle, Kelly M.",
+        "doi": "10.3389/fspor.2024.1349521",
+        "journal": "Frontiers in Sports and Active Living",
+        "keywords": "quantitative, user_study, immersion_perceived",
+        "title": "Exploring Actual and Perceived Levels of Physical Activity Intensity During Virtual Reality Active Games",
+        "type": "article",
+        "volume": "6",
+        "year": "2024"
+    },
+    "Naugle2025Youth": {
+        "abstract": "Compares Beat Saber, Thrill of the Fight, treadmill walking, and sedentary gaming; finds only boxing reaches moderate intensity.",
+        "author": "Naugle, Keith E. and others",
+        "doi": "10.1089/g4h.2024.0034",
+        "journal": "Games for Health Journal",
+        "keywords": "quantitative, user_study",
+        "number": "1",
+        "pages": "42--48",
+        "title": "Active Virtual Reality Games: Comparing Energy Expenditure, Game Experience, and Cybersickness to Traditional Gaming and Exercise in Youth Aged 8-12",
+        "type": "article",
+        "volume": "14",
+        "year": "2025"
+    }
+};
