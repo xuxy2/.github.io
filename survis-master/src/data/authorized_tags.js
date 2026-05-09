6d01003a77dd7d63eee43330b1717b8105bdab40
xuxy2 <111361103+xuxy2@users.noreply.github.com>
2026-05-09 21:04:11 +0100

    Add files via upload

---
+++ survis-master/src/data/authorized_tags.js
@@ -0,0 +1,12 @@
+const userDefinedAuthorizedTags = { 
+    // 在这里列出所有你在BibTeX文件中会用到的标签
+    "quantitative": { "description": "Employs quantitative methods such as heart rate or cognitive tests" },
+    "qualitative": { "description": "Uses qualitative data like thematic analysis of user interviews" },
+    "scoping_review": { "description": "A broad literature review mapping the research area" },
+    "user_study": { "description": "Controlled experiment involving human participants" },
+    "design_study": { "description": "Proposes and evaluates a new design or framework" },
+
+    // 如果某些标签有层级关系，可以用冒号:分隔
+    "immersion_perceived": { "description": "Studies focusing on users' perceived level of immersion" },
+    "immersion_measured": { "description": "Studies that attempt to measure immersion objectively" },
+} 
